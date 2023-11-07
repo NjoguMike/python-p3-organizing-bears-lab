2ab4e9fc632b42d1cd84305ecfeb954a910626a6
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-07 08:49:56 +0300

    Complete Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Mr.Chocolate",
+    20,
+    "M",
+    "brown",
+    "cool",
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Rowdy",
+    30,
+    "M",
+    "brown",
+    "angry",
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Tabitha",
+    25,
+    "F",
+    "white",
+    "sassy",
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Sergeant Brown",
+    40,
+    "M",
+    "black",
+    "stern",
+    0
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Melissa",
+    20,
+    "F",
+    "brown",
+    "melancholic",
+    0
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Grinch",
+    20,
+    "M",
+    "green",
+    "sanguin",
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Wendy",
+    10,
+    "f",
+    "black",
+    "happy",
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    ?,
+    20,
+    "F",
+    "brown",
+    "introvert",
+    0
+);
